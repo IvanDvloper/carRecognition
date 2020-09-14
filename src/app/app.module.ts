56739d6b17c17a267567fbc8f84376e658a18170
Ivan work <ivan.ugalde.dev@gmail.com>
2020-09-14 16:17:31 -0500

    first comit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AlprComponent} from './components/alpr/alpr.component';
+import {RouterModule} from "@angular/router";
+import {FaceComponent} from './components/face/face.component';
 
+import {FilePondModule, registerPlugin} from "ngx-filepond";
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import {HttpClientModule} from "@angular/common/http";
+import {AlprController} from "./controllers/alprController";
+registerPlugin(FilePondPluginFileValidateType);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlprComponent,
+    FaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FilePondModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'alpr', component: AlprComponent},
+      {path: 'face', component: FaceComponent},
+    ])
   ],
-  providers: [],
+  providers: [
+    AlprController
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
